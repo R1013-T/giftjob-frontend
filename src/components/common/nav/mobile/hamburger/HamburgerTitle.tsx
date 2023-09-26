2e7ba7d8c0ad0bdd34399fa9fe721730581602e1
R1013-T <nosuke1013@icloud.com>
2023-09-26 20:54:12 +0900

    update logo image url

--- src/components/common/nav/mobile/hamburger/HamburgerTitle.tsx
+++ src/components/common/nav/mobile/hamburger/HamburgerTitle.tsx
@@ -3,7 +3,12 @@ import Image from 'next/image'
 const HamburgerTitle = () => {
   return (
     <div className="flex justify-between items-end flex-wrap">
-      <Image src="/giftjob.svg" alt="giftjob" width={220} height={100} />
+      <Image
+        src="https://raw.githubusercontent.com/R1013-T/giftjob-frontend/fe5d2e1f7c3ddb0234ba345d102e012619b342f7/public/giftjob_logo.svg"
+        alt="giftjob"
+        width={220}
+        height={100}
+      />
       <p className="text-sm text-character font-normal">v 1.0</p>
     </div>
   )
