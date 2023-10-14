8ad600b3532cb014d2be3a2ed42c4b3d812367ea
R1013-T <nosuke1013@icloud.com>
2023-10-14 16:13:38 +0900

    fix add modal close event

--- src/components/main/company/modal/add/Add.tsx
+++ src/components/main/company/modal/add/Add.tsx
@@ -19,7 +19,7 @@ export default function CompanyAddModal(props: Props) {
   const router = useRouter()
   return (
     <AlertDialog open={props.isOpen} onOpenChange={props.setIsOpen}>
-      <AlertDialogContent>
+      <AlertDialogContent onCloseAutoFocus={() => router.back()} >
         <AlertDialogCancel
           className="absolute top-4 right-6 text-character p-0.5 bg-card border h-auto"
           onClick={() => {
