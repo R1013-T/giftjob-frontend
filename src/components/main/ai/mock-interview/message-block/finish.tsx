87dbc402aa44686d54dc37c53f08065c7b705f9b
R1013-T <nosuke1013@icloud.com>
2023-12-23 22:04:16 +0900

    update mock interview style

--- src/components/main/ai/mock-interview/message-block/finish.tsx
+++ src/components/main/ai/mock-interview/message-block/finish.tsx
@@ -18,7 +18,7 @@ export default function Finish({
         <InformationCircleIcon className="w-6 h-6 text-primary" />
         <p>模擬面接が終了しました。結果を表示します。</p>
       </div>
-      <div className="border rounded-md p-2 flex flex-col lg:flex-row gap-7 mt-3">
+      <div className="border rounded-md p-2 flex flex-col lg:flex-row gap-7 mt-3 relative">
         <ProgressCircle value={score} radius={50} strokeWidth={7}>
           <p className="text-2xl text-character font-semibold">{score}</p>
         </ProgressCircle>
