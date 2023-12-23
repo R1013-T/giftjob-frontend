e134ec12f73d4ba2e61bbb613d54beaf13d6ddba
R1013-T <nosuke1013@icloud.com>
2023-12-24 00:12:00 +0900

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,23 @@ import { monaSans } from '@/utils/fonts'
 
 export const metadata: Metadata = {
   title: 'GiftJob',
-  description: 'AI Job Hunting',
+  description: 'AI就活管理アプリ',
+  keywords: ['GiftJob', '就活補助', 'エントリーシート添削', '模擬面接', 'AI'],
+  openGraph: {
+    type: 'website',
+    locale: 'ja_JP',
+    url: 'https://giftjob-frontend.vercel.app/',
+    title: 'GiftJob',
+    description: 'AI就活管理アプリ',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/R1013-T/giftjob-frontend/894f2d1417c5af0d1ee1bf9a7506da229e893487/public/giftjob_icon.png',
+        width: 256,
+        height: 256,
+        alt: 'GiftJob',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
